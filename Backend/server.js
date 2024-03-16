f0fa30b8e13737487e6bb2eb0d75df636af7154e
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2024-03-16 17:31:48 +0530

    created

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const connectDB = require('./config/connectDatabase');
 const hospitalRoutes = require('./routes/hospitalRoutes');
+const morgan = require('morgan');
 
 connectDB();
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 app.use('/api/hospital', hospitalRoutes);
 
 app.get('/', (req, res) => {
