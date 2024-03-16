44c31201660ae0baf6486d54427be7bbbc8e87c2
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2024-03-16 21:02:22 +0530

    Added cors

--- Backend/server.js
+++ Backend/server.js
@@ -3,11 +3,13 @@ const connectDB = require('./config/connectDatabase');
 const hospitalRoutes = require('./routes/hospitalRoutes');
 const morgan = require('morgan');
 const Hospital = require('./models/hospitals.models');
+const cors = require('cors');
 
 connectDB();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api/hospital', hospitalRoutes);
