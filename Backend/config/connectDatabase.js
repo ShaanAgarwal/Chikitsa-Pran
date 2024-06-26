f0ef28164c51f294ccbe076dd849040bb46a220b
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2024-06-26 21:01:44 +0530

    Added Database URL

--- Backend/config/connectDatabase.js
+++ Backend/config/connectDatabase.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://shaanagarwalofficial:rOFG6oZeRHugwoFP@cluster0.o071o9c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect("mongodb+srv://agarwalshaan27:XU8vU1lyTOyo7PhW@cluster0.cojfvih.mongodb.net/chikitsa_pran?retryWrites=true&w=majority&appName=Cluster0");
         console.log(`Connected To MongoDB Database ${mongoose.connection.host}`);
     } catch (error) {
         console.log(`MongoDB Error: ${error}`.bgRed.white);
--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const connectDB = require('./config/connectDatabase');
 const hospitalRoutes = require('./routes/hospitalRoutes');
 const morgan = require('morgan');
-const Hospital = require('./models/hospitals.models');
 const cors = require('cors');
-const { contactUsSendEmailSingle } = require('./utils/emailSend');
 
 connectDB();
 
@@ -19,6 +17,6 @@ app.get('/', (req, res) => {
   return res.status(200).json({ message: "API is running properly", success: true });
 });
 
-app.listen(8080, () => {
-  console.log('Server is running on port 8080');
+app.listen(8081, () => {
+  console.log('Server is running on port 8081');
 });
